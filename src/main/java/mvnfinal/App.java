295db4f12a4ac4bc51a95bd966630a255c656c21
ashish <ath026@gmail.com>
2016-10-24 14:14:24 -0700

    addition of mvn project

---
+++ src/main/java/mvnfinal/App.java
@@ -0,0 +1,13 @@
+package mvnfinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
